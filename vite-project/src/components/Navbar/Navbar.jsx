02ec07f769a479c9224cbd03c4739cb4ff807c7f
immatchAA <143307155+immatchAA@users.noreply.github.com>
2025-01-09 17:51:24 +0800

    navbar updates 3

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -4,17 +4,25 @@ import {getImageUrl} from "../../utils";
 
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
     <nav className={styles.navbar}>
     <a className={styles.title} href = "/"> Portfolio </a>
     <div className = {styles.menu}>
-
-        <img className = {styles.menuBtn} 
-        src={getImageUrl("menuIcon.png")} 
+    <img className = {styles.menuBtn} 
+        src={
+            menuOpen
+            ? getImageUrl ("closeIcon.png")
+            : getImageUrl("menuIcon.png")
+        } 
         alt = "menu-button"
+        onClick={() => setMenuOpen (!menuOpen)}
         />
 
-        <ul className = {styles.menuItems}>
+        <ul className = {`${styles.menuItems} ${menuOpen && styles.menuOpen}` }
+        
+        >
             <li> <a href= "#about">About</a>   
             </li>
             <li> <a href= "#experience">Experience</a>   
