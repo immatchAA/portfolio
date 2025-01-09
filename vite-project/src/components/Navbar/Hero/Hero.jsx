b492b9f51d90ad9ec7d2897088ecaef7564085fd
immatchAA <143307155+immatchAA@users.noreply.github.com>
2025-01-09 19:51:27 +0800

    more updates. hero section. about me paragraph.

---
+++ vite-project/src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { getImageUrl } from '../../../utils';
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+    return (
+    <section className = {styles.container} > 
+            <div className = {styles.content}> 
+            <h1 className = {styles.title}>Hi, I'm Rich</h1>
+            <p className = {styles.description}>I'm a full stack developer. 
+                Reach out to me</p>    
+            <a href = "richellevillanueva.1234@gmail.com" className = {styles.contactBtn}>Contact Me</a>
+            </div>
+                <img src = {getImageUrl("girl.png")} 
+                alt = "Image me" 
+                className = {styles.heroImg}
+                />    
+                <div className = {styles.topBlur} />   
+                <div className = {styles.bottomBlur} />
+        </section>
+    );
+};
